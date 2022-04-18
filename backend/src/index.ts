1133a206301bf8b04715f147be5ad6b77f45a2e5
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2022-04-18 09:45:37 +0800

    🎨 Build before starting server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,11 @@
 import http from 'http';
 import mongoose from 'mongoose';
 import app from './app';
+import logger from './utils/logger';
 
-mongoose.connect('mongodb://localhost:27017/bookExchange');
-const server = http.createServer(app);
-server.listen(3001);
+mongoose.connect('mongodb://localhost:27017/bookExchange').then(() => {
+  const server = http.createServer(app);
+  server.listen(3001, () => {
+    logger.info('BookExchange server started at 3001');
+  });
+});
