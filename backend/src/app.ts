da1b03c243017c4b86c2490e7472893c471c7438
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2022-04-21 12:25:47 +0800

    🐛 Fix route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import cors from 'cors';
 import express from 'express';
+import path from 'path';
 import apiRouter from './router/apiRouter';
 import route from './utils/route';
 
@@ -7,5 +8,8 @@ const app = express();
 app.use(cors());
 app.use('/api', apiRouter);
 app.use(express.static(route.staticRoute));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(route.staticRoute, 'index.html'));
+});
 
 export default app;
