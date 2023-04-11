47c89de47213222fbdd59b81073242845199dfd6
Dustella <fdnoaivj@outlook.com>
2023-04-11 12:37:07 +0800

    ✨ add env for mongo url

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,16 @@ import mongoose from 'mongoose';
 import app from './app';
 import logger from './utils/logger';
 
-mongoose.connect('mongodb://localhost:27017/bookExchange').then(() => {
+// read username, password, address and port from environment variables
+const mongoUser = process.env.MONGO_USER ;
+const mongoPassword = process.env.MONGO_PASSWORD ;
+const prefix = mongoUser && mongoPassword ? `${mongoUser}:${mongoPassword}@` : '';
+const mongoAddress = process.env.MONGO_ADDRESS ?? "localhost";
+const mongoPort = process.env.MONGO_PORT ?? "27017";
+
+const mongoUrl = `mongodb://${prefix}${mongoAddress}:${mongoPort}/bookExchange`;
+
+mongoose.connect(mongoUrl).then(() => {
   const server = http.createServer(app);
   server.listen(3001, () => {
     logger.info('BookExchange server started at 3001');
