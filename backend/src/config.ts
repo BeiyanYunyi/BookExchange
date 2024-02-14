b89456fed24a55a15550634179a69b2924589bf1
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2024-02-14 23:10:28 +0800

    💚

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,9 +1,10 @@
 import dotenv from 'dotenv';
+import crypto from 'node:crypto';
 
 dotenv.config();
 
 export const saltRounds = process.env.SALT_ROUNDS ? Number(process.env.SALT_ROUNDS) : 10;
-export const jwtSecret = process.env.JWT_SECRETS!; // Must be set
-export const mongoUrl = process.env.MONGO_URL ?? 'mongodb://127.0.0.1:27017/bookExchange';
+export const jwtSecret =
+  process.env.MODE === 'test' ? crypto.randomUUID() : process.env.JWT_SECRETS!; // Must be set
 
 if (!jwtSecret) throw new Error('JWT_SECRET not found');
