3bf5ba7c8b7b040ff01016dc3a5c8900c8243ad8
TurboHsu <47477044+TurboHsu@users.noreply.github.com>
2024-04-15 11:03:06 +0800

    💄 shift usr avatar

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -5,7 +5,19 @@
         <div :class="$style.title" @click="router.push('/')">阅读益行</div>
       </template>
       <template #avatar>
-        <UserAvatar circle src="/头像圆.png" :class="$style.avatar" @click="router.push('/me')" />
+        <n-icon size="x-large">
+          <Book />
+        </n-icon>
+      </template>
+      <template #extra>
+        <n-button v-if="!authed" @click="router.push('/me')">登陆</n-button>
+        <UserAvatar
+          v-else
+          circle
+          src="/头像圆.png"
+          :class="$style.avatar"
+          @click="router.push('/me')"
+        />
       </template>
     </NPageHeader>
   </NCard>
@@ -15,13 +27,17 @@
 import { storeToRefs } from 'pinia';
 import { watch } from 'vue';
 import { useRouter } from 'vue-router';
+import { Book } from '@vicons/fa';
 import useLoadingStore from '../stores/loadingState';
 import UserAvatar from './UserAvatar.vue';
+import useAuthStore from '../stores/authState';
 
 const loadingBar = useLoadingBar();
 const router = useRouter();
 const loadingState = useLoadingStore();
+const authState = useAuthStore();
 const { loading } = storeToRefs(loadingState);
+const { authed } = storeToRefs(authState);
 
 watch(loading, (newLoading, oldLoading) => {
   if (newLoading) {
